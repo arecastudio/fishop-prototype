5888234a9e3fdbdf9752a7050642463af3bd5062
Admin <admin@Picos-iMac.local>
2020-07-16 13:25:47 +0700

    update

--- src/components/Collectionlist.js
+++ src/components/Collectionlist.js
@@ -2,7 +2,29 @@ import React from 'react';
 
 const Collectionlist=()=>{
     return(
-        <div className="collection-list">collection</div>
+        <div className="collection-list">
+            <h3>COLLECTION LIST</h3>
+            <div className="collection-items">
+                <div className="collection-item">
+                    Seafood Selections
+                </div>
+                <div className="collection-item">
+                    Seafood Selections
+                </div>
+                <div className="collection-item">
+                    Seafood Selections
+                </div>
+                <div className="collection-item">
+                    Seafood Selections
+                </div>
+                <div className="collection-item">
+                    Seafood Selections
+                </div>
+                <div className="collection-item">
+                    Seafood Selections
+                </div>
+            </div>
+        </div>
     )
 }
 
