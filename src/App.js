75522b418722dee39de40ea4c3f4b17e098a8791
Admin <admin@Picos-iMac.local>
2020-07-16 10:35:21 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  //Link
+} from 'react-router-dom';
+
+//import logo from './logo.svg';
+import './App.css';
+
+import Navtop from './components/Navtop';
+
+function App() {
+  return (
+    <div className="">
+      <Navtop/>
+      <div className="container">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
